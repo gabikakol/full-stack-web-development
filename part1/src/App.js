69adc4c55eebe77d629d1dd2e7fd5c78a0496090
gabikakol <kakol.gabrysia@gmail.com>
2023-02-16 13:08:22 +0200

    1.1 completed

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,5 +1,5 @@
 const App = () => {
-  console.log('test')
+  console.log('test App')
 
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -11,6 +11,7 @@ const App = () => {
 
   return (
     <div>
+      <h2>step1</h2>
       <h1>{course}</h1>
       <p>
         {part1} {exercises1}
@@ -22,8 +23,40 @@ const App = () => {
         {part3} {exercises3}
       </p>
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+
+      <br/>
+      <h2>step2</h2>
+      <Header course={course} />
+      <Content part={part1} exercises={exercises1}/>
+      <Content part={part2} exercises={exercises2}/>
+      <Content part={part3} exercises={exercises3}/>
+      <Total points={exercises1+exercises2+exercises3}/>
     </div>
   )
 }
 
+const Header = (props) => {
+  console.log('test Header2')
+
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  console.log('test Content2')
+
+  return (
+    <p>{props.part} {props.exercises}</p>
+  )
+}
+
 export default App
+
+const Total = (props) => {
+  console.log('test Total2')
+
+  return (
+    <p>Number of exercises {props.points}</p>
+  )
+}

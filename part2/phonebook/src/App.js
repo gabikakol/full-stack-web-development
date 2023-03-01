7645dea43d51d03896efd553f9204fe209d71b38
gabikakol <kakol.gabrysia@gmail.com>
2023-03-01 17:38:40 +0200

    2.7 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -8,7 +8,17 @@ const App = () => {
 
   const addName = (event) => {
     event.preventDefault()
-    setPersons(persons.concat({name: newName}))
+    console.log('button clicked', event.target)
+
+    const findName = persons.find(person => person.name === newName)
+
+    if (findName !== undefined) {
+      console.log('name repeated, not added')
+      window.alert(`${newName} is already added to phonebook`)
+    }
+    else {
+      setPersons(persons.concat({name: newName}))
+    }
     setNewName('')
   }
 

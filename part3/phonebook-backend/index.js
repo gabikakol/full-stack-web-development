c7c2c5506147d1722ecac920d770f99e98d64d16
gabikakol <kakol.gabrysia@gmail.com>
2023-03-09 20:42:25 +0200

    3.9 - 3.11 done

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('body',(req, res, next) => { return JSON.stringify(req.body) });
 const tiny = ':method :url :status :res[content-length] - :response-time ms :body'

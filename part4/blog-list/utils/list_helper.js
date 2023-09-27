9f8c7686cdcb06370bd7d2096ee9399542f620a9
gabikakol <gabriela.kakol@helsinki.fi>
2023-09-27 15:12:02 +0300

    4.4 done

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -4,7 +4,15 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const reducer = (sum, entry) => {
+        return sum + entry.likes
+    }
+
+    return blogs.reduce(reducer, 0)
+}
 
 module.exports = {
-    dummy
+    dummy, 
+    totalLikes
 }

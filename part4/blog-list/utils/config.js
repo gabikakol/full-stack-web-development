d3aed71dffbd5381563f0be38a02571bf190e3c2
gabikakol <gabriela.kakol@helsinki.fi>
2023-09-27 14:32:27 +0300

    4.2 update

--- part4/blog-list/utils/config.js
+++ part4/blog-list/utils/config.js
@@ -1,12 +1,7 @@
 require('dotenv').config()
 
-/* const PORT = process.env.PORT */
-/* const MONGODB_URI = process.env.MONGODB_URI
- */
-
-const PORT = 3003
-// REMEMBER PASSWORD!
-const MONGODB_URI = "mongodb+srv://kakolgab:<password>@fullstack.ilfyoih.mongodb.net/?retryWrites=true&w=majority"
+const PORT = process.env.PORT
+const MONGODB_URI = process.env.MONGODB_URI
 
 module.exports = {
   MONGODB_URI,
